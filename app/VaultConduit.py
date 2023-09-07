9626765d1fc2a69d8db2d6cbfe363b0d9e873cbb
joseph <53045966+jefrnc@users.noreply.github.com>
2023-09-07 12:08:43 -0300

    update vault client

--- app/VaultConduit.py
+++ app/VaultConduit.py
@@ -3,6 +3,9 @@ import logging
 import hvac
 from flask import Blueprint
 import os
+import requests
+import json
+import base64
 
 vault_conduit_bp = Blueprint('conduit', __name__)
 
@@ -15,9 +18,32 @@ class VaultConduit:
         self.TEAM = os.environ.get('team')
         self.PROJECT = os.getenv('project')
         self.SERVICE = os.environ.get('service')
+        self.VAULT_URL = os.environ.get('VAULT_URL', 'https://vault.devops-services-dev.frubana.com')
         try:
-            self.client = hvac.Client(url='https://vault.devops-services-dev.frubana.com')
-            self.client.token = self.vault_token
+            ECS_METADATA_URL = os.environ.get('ECS_CONTAINER_METADATA_URI_V4', 'http://169.254.170.2/v4')
+            task_role = json.loads(requests.get(f"{ECS_METADATA_URL}/task").text).get('TaskRoleArn')
+
+            self.client = hvac.Client(url=self.VAULT_URL)
+
+            # params = {
+            #     'role': task_role,
+            #     'iam_http_request_method': 'POST',
+            #     'iam_request_url': base64.b64encode('request-url-here'.encode()).decode('utf-8'),
+            #     'iam_request_body': base64.b64encode('request-body-here'.encode()).decode('utf-8'),
+            #     'iam_request_headers': base64.b64encode(json.dumps({'header1': 'value1', 'header2': 'value2'}).encode()).decode('utf-8'),
+            # }
+
+            response = self.client.auth_aws_iam(role=task_role)
+                                            # http_request_method=params['iam_http_request_method'],
+                                            # request_url=params['iam_request_url'],
+                                            # request_body=params['iam_request_body'],
+                                            # request_headers=params['iam_request_headers'])
+
+            if response['auth']:
+                self.client.token = response['auth']['client_token']
+            else:
+                logging.ERROR('No se pudo autenticar')
+
             if not self.client.is_authenticated():
                 logging.ERROR('No se pudo autenticar')
         except Exception as error:
