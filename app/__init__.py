9fcca7296f5338cd2131ac670ec62d4f8e881350
joseph <53045966+jefrnc@users.noreply.github.com>
2023-09-18 17:51:34 -0300

    update vault

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .healtcheck import healthcheck_bp
-from app.VaultConduit import vault_conduit_bp
+from app.VaultConduit import VaultConduit
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(healthcheck_bp)
     #app.register_blueprint(vault_conduit_bp)
+    
+    @app.route('/hello', methods=['GET'])
+    def hello():
+        return jsonify({"message": "Hello, world!"})
+    
+    @app.route('/vault', methods=['GET'])
+    def vault():
+        vault = VaultConduit()
+        return jsonify(vault.get_secret())
+
     return app
