0ad9e402839ccb842b4849874a5dbfd79d21075a
joseph <53045966+jefrnc@users.noreply.github.com>
2023-09-18 17:45:06 -0300

    remove test

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,5 @@ from app.VaultConduit import vault_conduit_bp
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(healthcheck_bp)
-    app.register_blueprint(vault_conduit_bp)
+    #app.register_blueprint(vault_conduit_bp)
     return app
